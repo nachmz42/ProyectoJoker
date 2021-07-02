2ffc91948811d998f50e63ccbd312a1e4baf67b1
nachmz42 <63515388+nachmz42@users.noreply.github.com>
2021-07-02 15:33:25 +0200

    Delete .metadata directory

--- .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Joker/STYLE/js/welcome.js
+++
@@ -1,8 +0,0 @@
-
-function abrir_barra() {
-    document.getElementById("SideBar").style.display = "block";
-}
-
-function cerrar_barra() {
-    document.getElementById("SideBar").style.display = "none";
-}
